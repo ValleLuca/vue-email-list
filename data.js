f7bcea277b62f3d79bff9d26ff927f5434340598
ValleLuca <valle.lucaAb@outlook.it>
2022-01-25 15:58:09 +0100

    Modificato esteticamente, eliminato bottone svuota

--- data.js
+++ data.js
@@ -1,24 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
+        mailTotali: null,
+        mailSelezionata: null,
         mail: [],
     },
 
     methods: {
         invioMail: function(){
-            for(let i = 0; i < 10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    this.mail.push({
-                        emailUtenti: response.data.response,
+            if(this.mailTotali != 10){
+                for(let i = 0; i < 10; i++){
+                    this.mailTotali = i + 1;
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.mail.push({
+                            emailUtenti: response.data.response,
+                        });
+    
+                    })
+                    .catch(function(error){
+                        console.log(error);
                     });
-
-                })
-                .catch(function(error){
-                    console.log(error);
-                });
+                }
             }
+            else
+            {
+                this.mail = [];
+                this.mailTotali = "";
+            }
+            
         },
+        selMail: function(indice){
+            this.mailSelezionata = indice;
+        }
     }
   })
 
