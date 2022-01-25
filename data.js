1fe63bc001b059c0a328d7fa8917d9a79f0ce199
ValleLuca <valle.lucaAb@outlook.it>
2022-01-25 15:01:59 +0100

    Conclusione spartana

--- data.js
+++ data.js
@@ -1,11 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        mail: [],
     },
+
     methods: {
-        
+        invioMail: function(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.mail.push({
+                        emailUtenti: response.data.response,
+                    });
+
+                })
+                .catch(function(error){
+                    console.log(error);
+                });
+            }
+        },
     }
   })
 
-  console.log("ciao");
+
+  /*
+  mounted: function(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.mail = response.data.response;
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+
+
+    },
+  */
